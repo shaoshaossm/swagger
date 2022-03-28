3f3562127896b4d3ed7db55d09eb620f581da7c3
shaoshao <1600767556@qq.com>
2022-03-28 12:58:43 +0800

    Swagger

--- swagger-demo/src/main/java/com/kuang/swagger/controller/HelloSwagger.java
+++ swagger-demo/src/main/java/com/kuang/swagger/controller/HelloSwagger.java
@@ -1,6 +1,7 @@
 package com.kuang.swagger.controller;
 
 import com.kuang.swagger.pojo.User;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
  * @date 2020/10/9 20:00
  */
-
+@Api(description = "helloSwagger类")
 @RestController
 public class HelloSwagger {
 
@@ -21,7 +22,11 @@ public class HelloSwagger {
     public String hello() {
         return "hello";
     }
-
+    @ApiOperation("注解应用")
+    @GetMapping(value = "/api")
+    public User api(@ApiParam("用户") User user) {
+        return user;
+    }
     //只要我们的接口中,返回值存在实体类,他就会扫描到Swagger中
     @PostMapping(value = "/user")
     public User user() {
