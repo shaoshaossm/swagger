3072c9ca6e094ec056da03585c69d836f40e29bc
shaoshao <1600767556@qq.com>
2022-04-01 19:07:43 +0800

    git test

---
+++ swagger-demo/src/main/java/com/kuang/swagger/demo/test.java
@@ -0,0 +1,13 @@
+package com.kuang.swagger.demo;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/4/1 19:02
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test2----test分支");
+    }
+}
